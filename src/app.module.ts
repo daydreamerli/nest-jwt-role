ffac2a63dd3a1f18d17274f6d820f816eeb4bfcf
daydreamerli <frankli84@outlook.com>
2022-09-01 23:43:53 +1200

    use http filter

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { APP_FILTER } from '@nestjs/core';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
+import { HttpExceptionFilter } from './filter/http-exception.filter';
 import { AuthModule } from './modules/auth/auth.module';
 import { CarsModule } from './modules/cars/cars.module';
 import { UsersModule } from './modules/users/users.module';
@@ -19,6 +21,12 @@ import { UsersModule } from './modules/users/users.module';
     CarsModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_FILTER,
+      useClass: HttpExceptionFilter,
+    },
+  ],
 })
 export class AppModule {}
