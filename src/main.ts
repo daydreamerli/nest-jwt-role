9e64fd150cf30e27b3a058d2694615b2e0b63dbd
daydreamerli <frankli84@outlook.com>
2022-09-01 23:45:34 +1200

    use http exception interceptor

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import 'dotenv/config';
 import { ConfigService } from '@nestjs/config';
+import { HttpExceptionFilter } from './filter/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -10,6 +11,7 @@ async function bootstrap() {
   const PREFIX = process.env.PREFIX || '/api';
 
   app.enableCors();
+  app.useGlobalFilters(new HttpExceptionFilter());
   app.setGlobalPrefix(PREFIX);
   await app.listen(PORT || 4000);
   console.log(`Server Application is running on port:  ${PORT}/${PREFIX}`);
