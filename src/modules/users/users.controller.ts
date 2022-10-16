343ac86f7a6a1ec6a28d08df7edc46791aa57410
daydreamerli <frankli84@outlook.com>
2022-10-16 20:02:46 +1300

    add guards to endpoint

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -6,16 +6,23 @@ import {
   Patch,
   Param,
   Delete,
+  UseGuards,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
+import { RolesGuard } from '../role/guards/role.guard';
+import { RolesAllowed } from '../role/role.decorator';
+import { Role } from '../role/role.enum';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post()
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @RolesAllowed(Role.ADMIN, Role.MANAGER)
   create(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
   }
@@ -26,11 +33,15 @@ export class UsersController {
   }
 
   @Patch(':id')
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @RolesAllowed(Role.ADMIN, Role.MANAGER)
   update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
     return this.usersService.update(+id, updateUserDto);
   }
 
   @Delete(':id')
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @RolesAllowed(Role.ADMIN, Role.MANAGER)
   remove(@Param('id') id: string) {
     return this.usersService.remove(+id);
   }
