0f6760985aabc883021caabe69a3dd6239306d4c
daydreamerli <frankli84@outlook.com>
2022-10-16 20:01:23 +1300

    add settings for HRM test

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ import 'dotenv/config';
 import { ConfigService } from '@nestjs/config';
 import { HttpExceptionFilter } from './filter/http-exception.filter';
 
+declare const module: any;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
@@ -13,6 +15,12 @@ async function bootstrap() {
   app.enableCors();
   app.useGlobalFilters(new HttpExceptionFilter());
   app.setGlobalPrefix(PREFIX);
+
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
+
   await app.listen(PORT || 4000);
   console.log(`Server Application is running on port:  ${PORT}/${PREFIX}`);
 }
