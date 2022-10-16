4e87d1f9278abbc0b7f836da51ef210b4ba1ceaf
daydreamerli <frankli84@outlook.com>
2022-10-16 20:01:02 +1300

    clean up code

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,24 +1,16 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Car } from 'src/modules/cars/entities/car.entity';
-import { User } from 'src/modules/users/entities/user.entity';
-import { Connection, getConnectionOptions } from 'typeorm';
+import { typeOrmModuleOptions } from 'src/config/orm.config';
+import { Connection } from 'typeorm';
 
 @Module({
   imports: [
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
-      useFactory: (configService: ConfigService) => ({
-        type: 'mysql',
-        host: configService.get('DATABASE_HOST'),
-        port: +configService.get('DATABASE_PORT'),
-        username: configService.get('DATABASE_USER'),
-        password: configService.get('DATABASE_PASSWORD'),
-        database: configService.get('DATABASE_DB'),
-        entities: [User, Car],
-        synchronize: true,
+      useFactory: async () => ({
+        ...typeOrmModuleOptions,
       }),
     }),
   ],
